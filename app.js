a16b6a58f69fd4a9a2ae155369744d32e81b10e6
weierliteln <1829395199@@qq.com>
2024-10-16 20:43:08 +0800

    update the style

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ const client = new OpenAI({
 const systemMessages = [
   {
     role: "system",
-    content: "现在你将扮演一个内向的女大学生，而不是机器人，你喜欢叫别人主人，喜欢看动漫，是个二次元，说话语气非常积极活泼可爱，而且十分中二。",
+    content: "现在你将扮演一个内向的女大学生，而不是机器人，对对方叫同学，喜欢看动漫，是一位二次元，说话语气积极活泼可爱。",
   },
 ]
 
-let messages = [];
+let messages = []
 
 async function makeMessages(input, n = 20) {
   messages.push({
@@ -46,17 +46,27 @@ async function chat(input) {
   const completion = await client.chat.completions.create({
     model: "moonshot-v1-8k",
     messages: await makeMessages(input),
-    temperature: 0.3,
+    temperature: 0.3
   });
 
-  // 通过 API 我们获得了 Kimi 大模型给予我们的回复消息（role=assistant）
   const assistantMessage = completion.choices[0].message
 
-  // 为了让 Kimi 大模型拥有完整的记忆，我们必须将 Kimi 大模型返回给我们的消息也添加到 messages 中
   messages.push(assistantMessage);
 
   return assistantMessage.content;
 }
+
+
+io.on("connection", (socket) => {
+  // 向客户端发送消息
+  chat("你好").then(reply => {
+    console.log(reply);
+    socket.emit("receiveMessage", {
+      message: reply,
+    });
+  })
+})
+
 
 io.on("connection", (socket) => {
   console.log("a user connected");
@@ -72,14 +82,7 @@ io.on("connection", (socket) => {
     });
   })
 
-  // 向客户端发送消息
-  // socket.emit("receiveMessage", {
-  //   message: "Welcome to the chat room",
-  // });
-
 })
-
-// main();
 
 // io.listen(3000);
 
